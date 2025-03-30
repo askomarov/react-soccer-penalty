868f8c9824194070892921f663109aa6640b34a7
Артем Комаров <komarov.ar@office.lan>
2025-03-30 16:24:46 +0200

    fix: правит файл deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-soccer-penalty/',
   plugins: [
     react(),
     tailwindcss()
